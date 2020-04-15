df557ff329969fb096e1ebe2e46ee225102b2466
pavel <pavel.stepanenko94@gmail.com>
2020-04-15 20:32:29 +0300

    fix: changed to material theme

--- src/components/ToggleMode.tsx
+++ src/components/ToggleMode.tsx
@@ -1,29 +1,29 @@
 import React, { FC, useContext, useMemo } from 'react'
-import IconButton from '@material-ui/core/IconButton'
+import { useTheme } from '@material-ui/core/styles'
 import Brightness2OutlinedIcon from '@material-ui/icons/Brightness2Outlined'
 import Brightness5Icon from '@material-ui/icons/Brightness5'
-import styled, { ThemeContext } from 'styled-components'
+import Box from '@material-ui/core/Box'
+import IconButton from '@material-ui/core/IconButton'
+import styled from 'styled-components'
 import { ModeContext } from '../context/ModeContext'
 
 const StyledIconButton = styled(IconButton)`
-	&& {
-		position: fixed;
-		right: 0;
-		top: 0;
-	}
+	position: fixed;
+	right: 0;
+	top: 0;
 `
 
 export const ToggleMode: FC = () => {
-	const { mode } = useContext(ThemeContext)
+	const { palette } = useTheme()
 	const { toggleMode } = useContext(ModeContext)
-	const icon = useMemo(
-		() => (mode === 'light' ? <Brightness2OutlinedIcon /> : <Brightness5Icon />),
-		[mode]
+	const modeButton = useMemo(
+		() => (
+			<StyledIconButton onClick={toggleMode} color="primary">
+				{palette.type === 'light' ? <Brightness2OutlinedIcon /> : <Brightness5Icon />}
+			</StyledIconButton>
+		),
+		[palette.type, toggleMode]
 	)
 
-	return (
-		<StyledIconButton onClick={toggleMode} color="secondary">
-			{icon}
-		</StyledIconButton>
-	)
+	return <Box>{modeButton}</Box>
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,42 @@
-import React, { useState, useCallback } from 'react'
+import React, { useState, useCallback, useMemo } from 'react'
 import ReactDOM from 'react-dom'
-import { ThemeProvider } from 'styled-components'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+import { StylesProvider } from '@material-ui/styles'
 import { App } from './components/App'
-import { GlobalStyle, defaultTheme } from './theme'
 import { ModeContext } from './context/ModeContext'
 
 const Main = () => {
-	const [theme, setTheme] = useState(defaultTheme)
+	const [themeMode, setThemeMode] = useState<'light' | 'dark'>('dark')
+	const MuiTheme = useMemo(
+		() =>
+			createMuiTheme({
+				palette: {
+					type: themeMode,
+					primary: {
+						main: themeMode === 'dark' ? '#EEE' : '#111',
+					},
+					background: {
+						default: themeMode === 'dark' ? '#111' : '#EEE',
+					},
+				},
+			}),
+		[themeMode]
+	)
 
 	const toggleMode = useCallback(() => {
-		const newTheme = theme.mode === 'dark' ? { mode: 'light' } : { mode: 'dark' }
-		setTheme(newTheme)
-	}, [theme.mode])
+		const newTheme = themeMode === 'dark' ? 'light' : 'dark'
+		setThemeMode(newTheme)
+	}, [themeMode])
 
 	return (
-		<ThemeProvider theme={theme}>
-			<GlobalStyle />
-			<ModeContext.Provider value={{ toggleMode }}>
-				<App />
-			</ModeContext.Provider>
+		<ThemeProvider theme={MuiTheme}>
+			<CssBaseline />
+			<StylesProvider injectFirst>
+				<ModeContext.Provider value={{ toggleMode }}>
+					<App />
+				</ModeContext.Provider>
+			</StylesProvider>
 		</ThemeProvider>
 	)
 }
